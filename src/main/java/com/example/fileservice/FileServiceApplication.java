f04ecfc18c62df1a30637bd5668738e968d8237e
Alisher Khudoyberdiev <aliwulug1307@gmail.com>
2023-08-26 10:14:50 +0500

    updated

--- src/main/java/com/example/fileservice/FileServiceApplication.java
+++ src/main/java/com/example/fileservice/FileServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.fileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class FileServiceApplication {
 
 	public static void main(String[] args) {
